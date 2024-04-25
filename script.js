03b91d19ab29929a4ef919e452e79690021c041f
Peter Yoon <peyoon@ucsd.edu>
2024-04-25 09:18:00 -0700

    backend init

--- script.js
+++ script.js
@@ -9,4 +9,14 @@ fileUpload.addEventListener('click', function() {
 
 fileStream.addEventListener('change', function(event) {
     const newFile = event.target.files[0];
-})
+    if (newFile && newFile.type == 'application/json') {
+        const r = new FileReader();
+        // will implement (first read raw JSON)
+    }
+});
+
+function display(taskData) {
+    const listContainer = document.getElementById("list");
+    listContainer.innerHTML = ''; // empty list
+    // foreach loop through tasks
+}
