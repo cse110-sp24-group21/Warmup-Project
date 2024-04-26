1c9aca637ddc944a319849c524aac4ebabf86e7e
IceGawd <avighna.kukreja@gmail.com>
2024-04-26 03:06:30 -0700

    Works for specific case
    Still needs
    - Array only functionality
    - Error display
    - Verification that the object has data
    - Verification that the data is formatted correct
    - Sorting of data

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 
 /* Upload JSON file and store in data directory */
-const fileUpload = document.getElementById("upload-file");
-const fileStream = document.getElementById("fileInput");
 
-fileUpload.addEventListener('click', function() {
-    fileStream.click();
-});
+function importData() {
+    let input = document.createElement('input');
+    input.type = 'file';
 
-fileStream.addEventListener('change', function(event) {
-    const newFile = event.target.files[0];
-    if (newFile && newFile.type == 'application/json') {
-        const r = new FileReader();
-        // will implement (first read raw JSON)
-    }
-});
+    input.onchange = _ => {
+        let file = input.files[0];
+        // Check if the file type is JSON or if the file name ends with ".json"
+        // The console logging is temporary and will be replaced with shown text
+        if (file.type === 'application/json' || file.name.endsWith('.json')) {
+            // We are given a JSON file and can't save it without some server side code, so I will just verify it and send it to the display function
+            const reader = new FileReader();
+
+            reader.onload = function(event) {
+                const jsonData = JSON.parse(event.target.result);
+                console.log(jsonData);
+
+                if (Array.isArray(jsonData)) {
+                    // Something else will need to be done
+                    console.log("Its an Array");
+                }
+                else if (jsonData !== null && typeof jsonData === 'object' && jsonData.constructor === Object) {
+                    if (!("title" in jsonData)) {
+                        // Add a temporary title
+                        jsonData["title"] = "Unnamed Task List";
+                    }
+                    display(jsonData);
+                }
+                else {
+                    // Something is wrong, return error
+                    console.log("I don't know what type it is");
+                }
+            };
+
+            reader.readAsText(file);
+        }
+        else {
+            console.log('File is not a JSON file.');
+        }
+    };
+
+    input.click();
+}
 
 function display(taskData) {
     const listContainer = document.getElementById("list");
