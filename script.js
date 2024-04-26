eb300ae1ca81a9f5c6aaf7d71609341f8e775e3e
IceGawd <avighna.kukreja@gmail.com>
2024-04-26 13:23:11 -0700

    Verification code

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
-
+var DEFAULT_TITLE_MESSAGE = "Unnamed Task List";
+var DATA_FIELDS = ["title", "dueDate", "label", "details"];
 /* Upload JSON file and store in data directory */
+
+function isPlainObject(obj) {
+    // Check if the object is not null and its type is "object"
+    if (obj !== null && typeof obj === 'object') {
+        // Get the object's prototype
+        const proto = Object.getPrototypeOf(obj);
+        // Check if the prototype is the default Object.prototype
+        return proto === Object.prototype || proto === null;
+    }
+    return false;
+}
+
+// NOTE: Currently doesn't sort
+function verifyAndSort(data) {
+    // returnData is the sorted data to return, or it will return null (Not used currently)
+    // returnData = [];
+    if (Array.isArray(data)) {
+        for (const element of data) {
+            if (isPlainObject(element)) {
+                // In future, it would add values into returnData in a sorted manner.
+                for (const field of DATA_FIELDS) {
+                    if (!(field in element)) {
+                        console.log("does not have field: " + field);
+                        return null;
+                    }
+                }
+            }
+            else {
+                console.log("not obj");
+                return null;
+            }
+        }
+    }
+    return data;
+    // return returnData;
+}
 
 function importData() {
     let input = document.createElement('input');
+    let toDisplay = null;
     input.type = 'file';
 
     input.onchange = _ => {
@@ -23,13 +61,31 @@ function importData() {
                 if (Array.isArray(jsonData)) {
                     // Something else will need to be done
                     console.log("Its an Array");
+                    if (verified(jsonData)) {
+                        // display(jsonData);
+                    }
+                    else {
+                        errorArea.innerHTML = 'Array is not in the correct format.';
+                    }
                 }
-                else if (jsonData !== null && typeof jsonData === 'object' && jsonData.constructor === Object) {
+                else if (isPlainObject(jsonData)) {
                     if (!("title" in jsonData)) {
                         // Add a temporary title
-                        jsonData["title"] = "Unnamed Task List";
+                        jsonData["title"] = DEFAULT_TITLE_MESSAGE;
                     }
-                    display(jsonData);
+                    if (!("data" in jsonData)) {
+                        errorArea.innerHTML = 'JSON has no data field.';
+                    }
+                    else {
+                        datas = verifyAndSort(jsonData["data"]);
+                        if (datas === null) {
+                            errorArea.innerHTML = 'Data field is not in the correct format.';
+                        }
+                        else {
+                            toDisplay = jsonData;
+                            toDisplay["data"] = datas;
+                        }
+                    }
                 }
                 else {
                     // Something is wrong, return error
@@ -45,6 +101,10 @@ function importData() {
     };
 
     input.click();
+
+    if (toDisplay != null) {
+        display(toDisplay);
+    }
 }
 
 function display(taskData) {
