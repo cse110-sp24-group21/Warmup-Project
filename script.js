2bd3a0f32f1a7a5a6a5522d0c5ccdc32a48808de
yue-hua-x <yux070@ucsd.edu>
2024-04-26 13:43:27 -0700

    Revert "finished display function"
    This reverts commit a1d1c68c9640a17d71cbe326f3f67b3057bf6a97.

--- script.js
+++ script.js
@@ -85,25 +85,8 @@ function upload() {
     }
 }
 
-/* Updating HTML based on the uploaded JSON */
 function display(taskData) {
-    //setting the title
-    const listTitle=document.getElementsByClassName("title");
-    listTitle[0].innerHTML=taskData.title;
-    //populating the list of tasks (?)
     const listContainer = document.getElementById("list");
     listContainer.innerHTML = ''; // empty list
-    const arrow = (containerElem.classList[1] == "light-mode")? 
-            '<img src="images/triangle-right-black.svg" alt="Task Icon "class="task-icon light-mode-icon right-icon" onclick="toggleIcon(this)">'
-            : '<img src="images/triangle-right-white.svg" alt="Task Icon "class="task-icon dark-mode-icon right-icon" onclick="toggleIcon(this)">';
-    for(let i=0; i<taskData.data.length; i++){
-        listContainer.innerHTML += '<li class="task">'
-                + arrow 
-                + '<input class="checkbox" type="checkbox">'
-                + '<h3 class="taskName">' + taskData.data[i].title + '</h3>'
-                + '<p class="due-date">'+ taskData.data[i].dueDate + '</p>';
-                + '<p class="description" style="display: none;">' + taskData.data[i].details + '</p>'
-                + '</li>';
-    }
+    // foreach loop through tasks
 }
-
